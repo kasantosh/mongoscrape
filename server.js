e147ddee5908b00ed32157ca67d3fa85259497b1
Santosh Kumar <kasantosh@yahoo.com>
2019-06-13 11:53:15 -0400

    finished with handlebars UI

--- server.js
+++ server.js
@@ -14,12 +14,51 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-  res.send("WElCOME");
-});
+// mongoose.connect();
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.nytimes.com/section/sports").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    var results = [];
+
+    $("div.css-10wtrbd").each(function(i, element) {
+      var title = $(element)
+        .children("h2")
+        .children("a")
+        .text();
+      var link = $(element)
+        .children("h2")
+        .children("a")
+        .attr("href");
+      var content = $(element)
+        .children("p")
+        .text();
+      var author = $(element)
+        .children()
+        .last()
+        .children(".css-9voj2j")
+        .children("span")
+        .text();
+
+      results.push({
+        title: title,
+        content: content,
+        author: author,
+        link: "https://www.nytimes.com" + link
+      });
+    });
+    res.json(results);
+    console.log(results);
+  });
+});
 
 app.listen(PORT, function() {
   console.log(`Listening on port ${PORT}`);
