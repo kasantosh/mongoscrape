9b6ee6a7798c1fca56ccb3da2b62df43bbf74cdf
Santosh Kumar <kasantosh@yahoo.com>
2019-06-17 15:49:02 -0400

    Server changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
+var db = require("./models");
 
 //Scraping packages
 var cheerio = require("cheerio");
@@ -9,8 +10,6 @@ var axios = require("axios");
 var PORT = process.env.PORT || 3000;
 
 var app = express();
-
-var db = require("./models");
 
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoscrapedb";
