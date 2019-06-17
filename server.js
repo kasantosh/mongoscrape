ad8b64fb600c8d7d1225b4cd592de852feb41aae
Santosh Kumar <kasantosh@yahoo.com>
2019-06-17 12:13:31 -0400

    made final changes to server

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ var db = require("./models");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoscrapedb";
-
 var PORT = process.env.PORT || 3000;
 
 var app = express();
@@ -18,10 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoscrapedb";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/", function(req, res) {
   db.Article.find()
